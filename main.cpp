40c28110c06b6b4f68324988321c4d263cc69a3f
MuffinHavlop <101418643+MuffinHavlop@users.noreply.github.com>
2023-11-11 08:55:16 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 #include <stdio.h>
 #include <cmath>
 
-
 using namespace std;
-
 
 void GiaiPhuongTrinh();
 void ptb1();
@@ -14,15 +12,12 @@ void HaiNghiemPhanBiet(float a, float b, float Delta);
 void Fibonacci();
 void Pythagoras();
 
-
 int main()
 {
     string Option;
-
     while (true)
     {
         cout << "Hay chon cac lua chon sau(q de thoat): \n" << "1.Giai phuong trinh\n" << "2.Pythagoras" << "3.Fibonacci" ; cin >> Option;
-
         if (Option == "1")
         {
             GiaiPhuongTrinh();
@@ -36,7 +31,6 @@ int main()
             Fibonacci();
         }
     }
-
     return 0;
 }
 
@@ -45,7 +39,6 @@ void GiaiPhuongTrinh()
 {
     string PhuongTrinh;
     cout << "Hay chon giai phuong trinh bac nhat (ptb1) hoac phuong trinh bac hai (ptb2): "; cin >> PhuongTrinh;
-
     if (PhuongTrinh == "ptb1")
     {
         ptb1();
@@ -65,15 +58,11 @@ void ptb1()
 {
     float a;
     float b;
-
     cout << "Nhap a: "; cin >> a;
     cout << "Nhap b: "; cin >> b;
     cout << "Phuong trinh cua ban la: " << a << "x + " << b << endl;
-
     float result;
-
     result = -b/(2*a);
-
     cout << "Ket qua cua phuong trinh la: " << result << endl;
 }
 
@@ -81,15 +70,11 @@ void ptb1()
 void ptb2()
 {
     float a, b, c;
-    
     cout << "Nhap a: "; cin >> a;
     cout << "Nhap b: "; cin >> b;
     cout << "Nhap c: "; cin >> c;
-
     cout << "Phuong trinh cua ban la: " << a << "x^2 + " << b << "x + " << c << endl;
-
     float SquaredDelta = sqrt(pow(b, 2) + 4*a*c);
-
     if (SquaredDelta < 0)
     {
         cout << "Phuong trinh vo nghiem\n";
@@ -103,17 +88,14 @@ void ptb2()
         cout << "Phuong trinh co hai nghiem phan biet\n";
         HaiNghiemPhanBiet(a, b, SquaredDelta);
     }
-
 }
 
 
 void HaiNghiemPhanBiet(float a, float b, float Delta)
 {
     float x1, x2;
-
     x1 = -b+Delta/(2*a);
     x2 = -b+Delta/(2*a);
-
     cout << "x1: " << ceil(x1) << endl;
     cout << "x2: " << ceil(x2) << endl;
 }
